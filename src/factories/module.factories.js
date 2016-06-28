65c6dbf4501b543739099bcbd77252d5b8acb1bd
Andrew Clinton <aclinton@anecka.com>
2016-06-28 15:59:40 -0400

    no longer need these factories

--- src/factories/module.factories.js
+++ src/factories/module.factories.js
@@ -1,8 +1,5 @@
 (function() {
     'use strict';
     angular
-        .module('app.factories', [
-        	'app.factory.metadata',
-        	'app.factory.queries'
-        ]);
+        .module('app.factories', []);
 })();
