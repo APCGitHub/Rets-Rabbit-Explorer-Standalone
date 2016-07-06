1c92386522115e79b9d6f6eff7252381ddbed8e1
Andrew Clinton <aclinton@anecka.com>
2016-07-06 13:07:27 -0400

    google maps config

--- src/app.config.js
+++ src/app.config.js
@@ -4,12 +4,18 @@
         .module('app.config', [])
         .config(Config);
 
-    Config.$inject = ["$stateProvider", "$urlRouterProvider", "ApiConfigProvider"];
+    Config.$inject = ["$stateProvider", "$urlRouterProvider", "ApiConfigProvider", "uiGmapGoogleMapApiProvider"];
 
-    function Config($stateProvider, $urlRouterProvider, ApiProvider) {
+    function Config($stateProvider, $urlRouterProvider, ApiProvider, uiGmapGoogleMapApiProvider) {
         ApiProvider.setBaseUrl("https://stage.retsrabbit.com/");
         ApiProvider.setClientId("retsrabbit");
         ApiProvider.setClientSecret("retsrabbit");
+
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyBzsdkBN_oP1rS262mZzEJiJRPrmfQ52LE',
+            v: '3.23', //defaults to latest 3.X anyhow
+            libraries: 'weather,geometry,visualization,drawing'
+        });
 
         $stateProvider
             .state('app', {
@@ -19,9 +25,9 @@
                 abstract: true
             })
             .state('app.explorer', {
-            	url: '/explorer',
-            	controller: 'ExplorerCtrl as Explorer',
-            	templateUrl: 'templates/home.html'
+                url: '/explorer',
+                controller: 'ExplorerCtrl as Explorer',
+                templateUrl: 'templates/home.html'
             });
 
         $urlRouterProvider.otherwise('/explorer');
