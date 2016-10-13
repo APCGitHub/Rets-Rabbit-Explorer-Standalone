766b08e699cddf9afd508c352d27442639a8c187
Andrew Clinton <ddrew.clinton@gmail.com>
2016-10-13 15:30:55 -0400

    new field service

--- src/v2/services/field.service.js
+++ src/v2/services/field.service.js
@@ -1,3 +1,31 @@
 /**
  * Created by aclinton on 10/13/16.
  */
+(function () {
+    'use strict';
+
+    angular
+        .module('app.service.fields', [])
+        .service('FieldService', Service);
+
+    Service.$inject = [];
+
+    function Service() {
+        this.parse = function (field_string) {
+            if(!field_string || !field_string.length)
+                return '';
+
+            var parts = field_string.split(',');
+
+            parts = parts.map(function (field){
+                return field.trim();
+            });
+
+            parts = parts.filter(function(field){
+                return field.length;
+            });
+
+            return {value: parts.join('|'), parts: parts};
+        };
+    }
+})();
