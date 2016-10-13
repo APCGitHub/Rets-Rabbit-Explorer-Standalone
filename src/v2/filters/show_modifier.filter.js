a3e92c23787039e63cd095726cf9ef7271833a98
Andrew Clinton <ddrew.clinton@gmail.com>
2016-10-13 15:30:42 -0400

    handle multi field filtering

--- src/v2/filters/show_modifier.filter.js
+++ src/v2/filters/show_modifier.filter.js
@@ -8,54 +8,77 @@
         .module('app.filter.show_modifier', [])
         .filter('showModifier', Filter);
 
-    function Filter() {
+    Filter.$inject = ['FieldService', 'MetadataService'];
+
+    function Filter(FieldService, MetadataService) {
         return function (mods, field, metadata) {
-            if(!field || field === '')
+            if(!field || field === '' || !metadata || !metadata.length)
                 return mods;
 
+            //build list of allowed modifiers
             var new_mods = [];
+
+            //field may be comma separated list
+            var field_data = FieldService.parse(field);
+
+            //store each field's meta in an array
+            var metas = [];
+
+            //check through each field to find it's meta
+            for(var j = 0; j < field_data.parts.length; j++) {
+                for(var jj = 0, len2 = metadata.length; jj < len2; jj++){
+                    if(metadata[jj].Name === field_data.parts[j]){
+                        metas.push(metadata[jj]);
+                        break;
+                    }
+                }
+            }
+
+            //at least one didn't match so we just return current_mods
+            if(metas.length != field_data.parts.length){
+                return new_mods;
+            }
+
+            //all field metas should have the same type
+            if(metas.length > 1){
+                var first_type = MetadataService.findKey(metas[0].Type);
+
+                for(var i = 1; i < metas.length; i++){
+                    if(first_type !== MetadataService.findKey(metas[i].Type))
+                        return new_mods;
+                }
+            }
 
             for(var i = 0; i < mods.length; i ++) {
                 var _mod = mods[i];
                 var passed = false;
 
-                if (!metadata || !metadata.length) {
-                    new_mods.push(_mod);
-                    continue;
+                var _field = metas[0];
+                var _name = _field.Type;
+
+                //number types
+                if (_mod.for === 'number') {
+                    if (_name.toLowerCase() === 'integer')
+                        passed = true;
+
+                    if (_name.toLowerCase() === 'numeric')
+                        passed = true;
+
+                    if (_name.toLowerCase() === 'decimal')
+                        passed = true;
                 }
 
-                for (var j = 0, len2 = metadata.length; j < len2; j++) {
-                    if (metadata[j].Name == field) {
-                        var _field = metadata[j];
-                        var _name = _field.Type;
+                //date types
+                if(_mod.for === 'date'){
+                    if (_name.toLowerCase() === 'date')
+                        passed = true;
+                }
 
-                        //number types
-                        if (_mod.for === 'number') {
-                            if (_name.toLowerCase() === 'integer')
-                                passed = true;
-
-                            if (_name.toLowerCase() === 'numeric')
-                                passed = true;
-
-                            if (_name.toLowerCase() === 'decimal')
-                                passed = true;
-                        }
-
-                        //date types
-                        if(_mod.for === 'date'){
-                            if (_name.toLowerCase() === 'date')
-                                passed = true;
-                        }
-
-                        //string types
-                        if (_mod.for === 'string') {
-                            if (_name.toLowerCase().includes('character') ||
-                                _name.toLowerCase().includes('text'))
-                                passed = true;
-                        }
-
-                        break;
-                    }
+                //string types
+                if (_mod.for === 'string') {
+                    if (_name.toLowerCase().includes('character') ||
+                        _name.toLowerCase().includes('text'))
+                        passed = true;
                 }
 
                 if (passed)
