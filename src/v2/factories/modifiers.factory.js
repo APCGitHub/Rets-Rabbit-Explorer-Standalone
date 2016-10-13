3919bc48ebdb6de8149c2b2dae4229062ee39ee3
Andrew Clinton <ddrew.clinton@gmail.com>
2016-10-13 13:12:01 -0400

    new factories

---
+++ src/v2/factories/modifiers.factory.js
@@ -0,0 +1,90 @@
+/**
+ * Created by aclinton on 10/13/16.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('app.factory.modifiers', [])
+        .factory('V1ModifiersFactory', Factory);
+
+    Factory.$inject = [];
+
+    function Factory() {
+        var factory = {
+            all: _all,
+            map: _map
+        };
+
+        return factory;
+
+        function _all() {
+            var m = [
+                {
+                    name: 'Equals',
+                    value: '=',
+                    for: 'string'
+                },
+                {
+                    name: 'Starts With',
+                    value: ':startswith',
+                    for: 'string'
+                }, {
+                    name: 'Ends With',
+                    value: ':endswith',
+                    for: 'string'
+                }, {
+                    name: 'Contains',
+                    value: ':indexof',
+                    for: 'string'
+                }, {
+                    name: 'No Case',
+                    value: ':nocase',
+                    for: 'string'
+                },/* number modifiers */ {
+                    name: 'Greater',
+                    value: '+',
+                    for: 'number'
+                }, {
+                    name: 'Less',
+                    value: '-',
+                    for: 'number'
+                }, {
+                    name: 'Between',
+                    value: '<->',
+                    for: 'number'
+                }, {
+                    name: 'Equals',
+                    value: '=',
+                    for: 'number'
+                }, /* date modifiers */ {
+                    name: 'Greater',
+                    value: '+',
+                    for: 'date'
+                }, {
+                    name: 'Less',
+                    value: '-',
+                    for: 'date'
+                }, {
+                    name: 'Between',
+                    value: '<->',
+                    for: 'date'
+                }, {
+                    name: 'Equals',
+                    value: '=',
+                    for: 'date'
+                }
+            ];
+
+            return m;
+        }
+
+        function _map() {
+            return {
+                'string': ['character varying', 'text'],
+                'number': ['numeric', 'integer'],
+                'date': ['date']
+            }
+        }
+    }
+})();
---
+++ src/v2/factories/v1_queries.factory.js
@@ -0,0 +1,89 @@
+/**
+ * Created by aclinton on 10/12/16.
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('app.factory.v1_queries', [])
+        .factory('V1QueryFactory', Factory);
+
+    Factory.$inject = [];
+
+    function Factory() {
+        var factory = {
+            all: _all
+        };
+
+        return factory;
+
+        function _all(){
+            var q = [{
+                title: 'Price: Greater Than',
+                description: 'This is a simple price check for any listings having ListPrice greater than $95,000',
+                query: [{
+                    field: 'ListPrice',
+                    value: {
+                        left: '95000',
+                        right: ''
+                    },
+                    modifier: '+'
+                }]
+            }, {
+                title: 'Price: Less Than',
+                description: 'This is a simple price check for any listing having ListPrice less than $55,000',
+                query: [
+                    {
+                        field: 'ListPrice',
+                        value: {
+                            left: '55000',
+                            right: ''
+                        },
+                        modifier: '-'
+                    }
+                ]
+            }, {
+                title: 'Price: Between',
+                description: 'This is a simple price check for any listings having a ListPrice between $78,000 and $85,000',
+                query: [
+                    {
+                        field: 'ListPrice',
+                        value: {
+                            left: '78000',
+                            right: '85000'
+                        },
+                        modifier: '<->'
+                    }
+                ]
+            }, {
+                title: 'Date: Between',
+                description: 'This is a simple date search for listings having ListingContractDate between 2016/04/01 and 2016/05/26',
+                query: [
+                    {
+                        field: 'ListingContractDate',
+                        value: {
+                            left: '2016/04/01',
+                            right: '2016/05/26'
+                        },
+                        modifier: '<->'
+                    }
+                ]
+            }, {
+                title: 'Multiple Values',
+                description: 'This is a more complex query searching for listings which have the city names Dublin or Worthington',
+                query: [
+                    {
+                        field: 'City',
+                        value: {
+                            left: 'Dublin, Worthington',
+                            right: ''
+                        },
+                        modifier: '='
+                    }
+                ]
+            }];
+
+            return q;
+        }
+    }
+})();
