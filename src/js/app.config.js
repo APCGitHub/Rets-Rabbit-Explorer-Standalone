8893fa0ab103ed9e9d9f84c319ece07337adf142
Andrew Clinton <ddrew.clinton@gmail.com>
2016-10-14 13:26:51 -0400

    moved out from under v2 directory

--- src/js/app.config.js
+++ src/js/app.config.js
@@ -4,12 +4,16 @@
         .module('app.config', [])
         .config(Config);
 
-    Config.$inject = ["$stateProvider", "$urlRouterProvider", "$locationProvider", "ApiConfigProvider"];
+    Config.$inject = ["$stateProvider", "$urlRouterProvider", "ApiConfigProvider"];
 
-    function Config($stateProvider, $urlRouterProvider, $locationProvider, ApiProvider) {
+    function Config($stateProvider, $urlRouterProvider, ApiProvider) {
         ApiProvider.setBaseUrl("https://stage.retsrabbit.com/");
         ApiProvider.setClientId("retsrabbit");
         ApiProvider.setClientSecret("retsrabbit");
+
+        // ApiProvider.setBaseUrl('http://retsrabbit.app/');
+        // ApiProvider.setClientId('E1bi6hyy7nLxjlicqE2cDhyUykmA11KPoK9cSbr');
+        // ApiProvider.setClientSecret('x8EZaK74sYmkQLOk4CJBhwLJP0McajzKro6RY6j');
 
         $stateProvider
             .state('app', {
--- src/js/controllers/explorerv2.controller.js
+++ src/js/controllers/explorerv2.controller.js
@@ -4,9 +4,9 @@
         .module('app.controller.explore.v2', [])
         .controller('ExploreV2Ctrl', Controller);
 
-    Controller.$inject = ['$scope', '$window', '$document', '$timeout', 'MetadataFactory', 'QueryFactory', 'RRAuthFactory'];
+    Controller.$inject = ['$scope', '$document', 'MetadataFactory', 'QueryFactory', 'RRAuthFactory'];
 
-    function Controller($scope, $window, $document, $timeout, MetadataFactory, QueryFactory, AuthFactory) {
+    function Controller($scope, $document, MetadataFactory, QueryFactory, AuthFactory) {
     	var vm = this;
 
     	init();
@@ -44,7 +44,6 @@
             vm.filterMeta = _filterMeta;
             vm.fillQuery = _fillQuery;
             vm.toggleQueryInfo = _toggleQueryInfo;
-            vm.startTutorial = _startTutorial;
     	}
 
         /* --- FUNCTIONS --- */
@@ -79,13 +78,6 @@
         function _toggleQueryInfo() {
             vm.data.query_info.hidden = !vm.data.query_info.hidden;
             //do other stuff if necessary
-        }
-
-        function _startTutorial() {
-            $document.scrollTopAnimated(0, 200).then(function() {
-                $scope.data.tutorial.active = true;
-                $scope.data.tutorial.example_queries = true;
-            });
         }
     }
 })();
