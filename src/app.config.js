f6d8a8b5a46dd6fe90c27c2c1a6fde557a2ec50b
Andrew Clinton <ddrew.clinton@gmail.com>
2016-10-18 16:33:13 -0400

    for testing

--- src/app.config.js
+++ src/app.config.js
@@ -7,9 +7,13 @@
     Config.$inject = ["$stateProvider", "$urlRouterProvider", "ApiConfigProvider", "uiGmapGoogleMapApiProvider"];
 
     function Config($stateProvider, $urlRouterProvider, ApiProvider, uiGmapGoogleMapApiProvider) {
-        ApiProvider.setBaseUrl("https://stage.retsrabbit.com/");
-        ApiProvider.setClientId("retsrabbit");
-        ApiProvider.setClientSecret("retsrabbit");
+        // ApiProvider.setBaseUrl("https://stage.retsrabbit.com/");
+        // ApiProvider.setClientId("retsrabbit");
+        // ApiProvider.setClientSecret("retsrabbit");
+
+        ApiProvider.setBaseUrl('http://retsrabbit.app/');
+        ApiProvider.setClientId('E1bi6hyy7nLxjlicqE2cDhyUykmA11KPoK9cSbr');
+        ApiProvider.setClientSecret('x8EZaK74sYmkQLOk4CJBhwLJP0McajzKro6RY6j');
 
         uiGmapGoogleMapApiProvider.configure({
             key: 'AIzaSyBzsdkBN_oP1rS262mZzEJiJRPrmfQ52LE',
